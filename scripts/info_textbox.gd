3e5ac72e360708ad26fbb09442e32e75a81bccb3
kennethbchen <kennethbchen@gmail.com>
2024-01-29 18:39:24 -0500

    Made info textbox animation work with any screen size probably

--- scripts/info_textbox.gd
+++ scripts/info_textbox.gd
@@ -4,14 +4,14 @@ extends Control
 
 @onready var textbox: RichTextLabel = $RichTextLabel
 
+# Position is normalized viewport position (0 - 1)
 var original_position: Vector2 = Vector2.ZERO
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	visible = false
 	EventBus.player_entered_text_info.connect(on_display_text)
 	EventBus.player_exited_text_info.connect(on_hide_text)
-	original_position = position
+	original_position = position / get_viewport_rect().size
 	on_hide_text()
 	
 func on_display_text(text: String):
@@ -26,13 +26,17 @@ func make_visible():
 	var t = get_tree().create_tween()
 	t.set_ease(Tween.EASE_OUT)
 	t.set_trans(Tween.TRANS_CUBIC)
-	t.tween_property(self, "position", original_position, animation_time)
+	t.tween_property(self, "position", original_position * get_viewport_rect().size, animation_time)
 	
 func make_hidden():
+	
+	get_rect()
+	var hide_position = original_position * get_viewport_rect().size + Vector2(0, get_viewport_rect().size.y * 0.4)
+	print(hide_position)
 	var t = get_tree().create_tween()
 	t.set_ease(Tween.EASE_IN)
 	t.set_trans(Tween.TRANS_CUBIC)
-	t.tween_property(self, "position", original_position - Vector2(0, -200), animation_time)
+	t.tween_property(self, "position", hide_position, animation_time)
 	t.finished.connect(func():
 		textbox.text = ""
 		visible = false

c1946f42a7127c8852f29ed4cc13e81e768110a0
kennethbchen <kennethbchen@gmail.com>
2024-04-29 20:11:59 -0400

    Started implementing pause menu

---
+++ scripts/pause_menu.gd
@@ -0,0 +1,14 @@
+extends NinePatchRect
+
+
+signal pause_menu_opened()
+signal pause_menu_closed()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass

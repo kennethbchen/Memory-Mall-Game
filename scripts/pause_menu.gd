3169f043e85621b5a81f3636fe53c74352b7908b
kennethbchen <kennethbchen@gmail.com>
2024-05-04 15:26:50 -0400

    implemented wigglyness setting

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,6 +1,10 @@
 extends NinePatchRect
 
 
+@export var wiggle_post_process_material: ShaderMaterial
+
+@export var wiggle_outline_materials: Array[ShaderMaterial]
+
 signal pause_menu_opened()
 signal pause_menu_closed()
 
@@ -34,4 +38,13 @@ func on_slider_changed(value: float, name: String):
 		print(name, " ", AudioServer.get_bus_volume_db(AudioServer.get_bus_index(name)))
 	
 	if name == "Wiggle":
+		
+		"""
+		for mat in wiggle_outline_materials:
+			mat.set_shader_parameter("noise_sensitivity", remap(value, 0, 100, 0, 0.05))
+		"""
+		
+		var wiggle_range = remap(value, 0, 100, 0, 0.007)
+		wiggle_post_process_material.set_shader_parameter("wiggle_range", Vector2(wiggle_range, wiggle_range) )
+		
 		print(name, " ", value)

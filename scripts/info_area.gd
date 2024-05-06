c7a4917b4c55d02963cea0c9f3900a9f5eee9a55
kennethbchen <kennethbchen@gmail.com>
2024-05-06 15:04:42 -0400

    Fixed issue were info areas were detecting the environment

--- scripts/info_area.gd
+++ scripts/info_area.gd
@@ -9,8 +9,12 @@ func _ready():
 	body_exited.connect(_on_body_exited)
 	
 func _on_body_entered(body: Node3D):
-	EventBus.player_entered_text_info.emit(text)
-	activated.emit()
+	
+	if body is Player:
+		EventBus.player_entered_text_info.emit(text)
+		activated.emit()
 
 func _on_body_exited(body: Node3D):
-	EventBus.player_exited_text_info.emit()
+	
+	if body is Player:
+		EventBus.player_exited_text_info.emit()
